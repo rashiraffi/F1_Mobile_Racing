0c81839b2baa98d7e513be54676c1c4f13472f67
Rashi M <49615234+rashiraffi@users.noreply.github.com>
2023-08-28 15:07:42 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -18,7 +18,7 @@ def main():
         data = json.load(f)
 
     
-    # uri = "mongodb+srv://rashiraffi:tfVhLu9Q7xHsvjma@digid.1mdserk.mongodb.net/?retryWrites=true&w=majority"
+    # uri = "mongodb+srv://rashiraffi:pwd@digid.1mdserk.mongodb.net/?retryWrites=true&w=majority"
     # # Create a new client and connect to the server
     # client = MongoClient(uri, server_api=ServerApi('1'))
     # # Send a ping to confirm a successful connection
